f8d93fb9dff8fb7ace66ad04cdd0df8f1ab196a6
Cr4zyRi0s <Cr4zyRi0s.channel@gmail.com>
2021-12-15 19:23:09 +0100

    committone

--- scripts/process_dataset.py
+++ scripts/process_dataset.py
@@ -3,7 +3,10 @@ import random
 import cv2
 import csv
 import numpy as np
-import pickle 
+import pickle
+
+from tqdm import tqdm
+from skimage.util import random_noise
 
 def count_age_groups(filenames):
     age_counts = {}
@@ -16,17 +19,102 @@ def count_age_groups(filenames):
 
     return sorted(age_counts.items())
 
+def count_classes(labels):
+    counts = [0,0,0]
+    positive_counts = [0,0,0]
+    for _,v in labels.items():
+        if v[0] == '1':
+            positive_counts[0] += 1
+        else:
+            counts[0] += 1
+        if v[1] == '1':
+            positive_counts[1] += 1
+        else:
+            counts[1] += 1
+        if v[2] == '1':
+            positive_counts[2] += 1
+        else:
+            counts[2] += 1
+    counts.extend(positive_counts)
+
+    return counts
+
+
 def preprocessing(img):
     return img
 
-def augmentation(group_imgs, group_filenames,target_number):
+def _blur_pass(img):
+    return cv2.GaussianBlur(img, (3,3), 0)
+
+def _noise_pass(img):
+    float_img = random_noise(img)
+    return np.array(255*float_img, dtype = 'uint8')
+
+def _brightness_shift_pass(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    val = 0
+    rand = random.randint(-40,40)
+    for x in range(v.shape[0]):
+        for y in range(v.shape[1]):
+            val = v[x][y]
+            if rand >= 0:
+                v[x][y] = min(255, val + rand)
+            else:
+                v[x][y] = max(0, val + rand)
+
+    final_hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img
+
+def contrast_shift_pass(img):
+    
+    
+    return img
+'''
+def add_googles_pass():
+    pass
+
+def add_beard_pass():
+    pass
+'''
+def get_aug_processes():
+    return  [_blur_pass, _noise_pass, _brightness_shift_pass, contrast_shift_pass]
+
+def augmentation(group_imgs, group_filenames,labels,target_number):
+    group_size = len(group_imgs)
     factor = target_number/len(group_imgs)
     perc = factor - 1.0
+    aug_processes = get_aug_processes()
 
-    if perc <= 1.0:
-        pass
+    gen_img = None
+    gen_filename = None
+    
+    c = 0
+    #d = {}
+    if perc < 1.0:
+        random_indexes = random.sample(range(0,group_size),k= target_number - group_size)
+        for i in random_indexes:
+            split_filename = group_filenames[i].split(".")
+            split_filename.insert(-1,str(c))
+            gen_filename = '.'.join(split_filename)
+
+            aug_proc = random.sample(aug_processes, k = 1)[0]
+            #d[gen_filename] = aug_proc.__name__
+            gen_img = aug_proc(group_imgs[i])
+            group_imgs.append(gen_img)
+            group_filenames.append(gen_filename)
+            labels[gen_filename] = labels[group_filenames[i]]
+
+            c += 1
     else:
         pass
+        
+    
+
+    return group_imgs,group_filenames, labels
+
+
 
 
 
@@ -45,7 +133,9 @@ if __name__ == "__main__":
         reader = csv.reader(csvfile, delimiter = ",")
         for row in reader:
             label_dict[row[0]] = tuple(row[1:])
-
+    ret = count_classes(label_dict)
+    print(ret)
+    exit()
 
     try:
         os.mkdir(path_train)
@@ -70,13 +160,15 @@ if __name__ == "__main__":
             target = n_per_age
             if n_per_age / n > max_augmentation:
                 target = n * max_augmentation
-            aug_imgs,aug_filenames = augmentation(group_imgs,group_filenames,target)
-            for i in range(len(group_imgs)):
+            aug_imgs, aug_filenames, aug_labels = augmentation(group_imgs,group_filenames, label_dict, target)
+            for i in range(len(aug_imgs)):
                 cv2.imwrite(os.path.join(path_train,aug_filenames[i]), aug_imgs[i])
+            break
         elif n > n_per_age:
             random_indexes = random.sample(range(0,n),k=n_per_age)
             for i in random_indexes:
-                cv2.imwrite(os.path.join(path_train, group_filenames[i]), group_imgs[i])
+                cv2.imwrite(os.path.join(path_train,group_filenames[i]), group_imgs[i])
+       
 
 
     
