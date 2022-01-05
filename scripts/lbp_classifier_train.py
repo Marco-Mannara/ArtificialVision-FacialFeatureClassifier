f276d6602a3b7e7d49e66398bbf182d8067f3f86
Cr4zyRi0s <Cr4zyRi0s.channel@gmail.com>
2022-01-05 23:59:13 +0100

    commit

--- scripts/lbp_classifier_train.py
+++ scripts/lbp_classifier_train.py
@@ -4,22 +4,11 @@ from collections import OrderedDict
 from Dataset import Dataset
 from FacialFeaturesClassifier import FFClassifier
 
-landm_ids_68 = OrderedDict([
-("mouth", (48, 68)),
-("inner_mouth", (60, 68)),
-("right_eyebrow", (17, 22)),
-("left_eyebrow", (22, 27)),
-("right_eye", (36, 42)),
-("left_eye", (42, 48)),
-("nose", (27, 36)),
-("jaw", (0, 17))
-])
-
 if __name__ == "__main__":
 	path_dataset = "dataset"
-	train_dset = Dataset("train", path_dataset) 
+	train_dset = Dataset("train", path_dataset)  
 
-	train_dset.load("train", "train_label.csv")
+	train_dset.load("train0", "train_label0.csv")
 
 	try:
 		os.mkdir("trained_models")
@@ -30,3 +19,4 @@ if __name__ == "__main__":
 	X,y = train_dset.to_lists()
 	classifier.fit(X,y)
 	classifier.save()
+	
